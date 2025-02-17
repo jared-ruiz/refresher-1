0b3594b91bfa3d8530b83ccd7f2675b1d5255ec7
Jared Ruiz <jared707@gmail.com>
2025-02-17 15:31:29 -0600

    added dynamically loading text on home page based on Math function.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+//array of descriptors to dynamically load
+const reactDesciptions = ["Fundamental", "Crucial", "Core"];
+
+//math function to get a random into from the 3 items in the reactDescriptions array
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  //took logic out of curly brackets and placed it above to lean out the jsx
+  const description = reactDesciptions[genRandomInt(2)]
+
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
