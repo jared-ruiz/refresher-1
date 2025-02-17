d2f984c01357fb0967ca1148a69c454f4b607c60
Jared Ruiz <jared707@gmail.com>
2025-02-17 15:35:15 -0600

    Imported react image to dynamically load it as well.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from './assets/react-core-concepts.png';
+
 //array of descriptors to dynamically load
 const reactDesciptions = ["Fundamental", "Crucial", "Core"];
 
@@ -12,7 +14,7 @@ function Header() {
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
