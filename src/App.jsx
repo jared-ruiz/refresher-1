ad98588ebe28906a0ef42bd5889306406ace9f84
Jared Ruiz <jared707@gmail.com>
2025-02-17 23:12:40 -0600

    Added components directory and sub directories for individual components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,6 @@
-import reactImg from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data.js';
-
-//array of descriptors to dynamically load
-const reactDesciptions = ["Fundamental", "Crucial", "Core"];
-
-//math function to get a random into from the 3 items in the reactDescriptions array
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  //took logic out of curly brackets and placed it above to lean out the jsx
-  const description = reactDesciptions[genRandomInt(2)]
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  )
-}
-
-function CoreConcept({image, title, description}) {
-  return(
-    <li>
-      <img src={image} alt={title}></img>
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  )
-}
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 
 //added props to CoreConcepts component. Since I am familiar with this concept, I destructured them to make more sense visably for myself
 //... spread operator will pull all the key value pairs from the core_concepts object array and give the same results as the one above
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import reactImg from '../../assets/react-core-concepts.png';
+
+//array of descriptors to dynamically load
+const reactDesciptions = ["Fundamental", "Crucial", "Core"];
+
+//math function to get a random into from the 3 items in the reactDescriptions array
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export default function Header() {
+    //took logic out of curly brackets and placed it above to lean out the jsx
+    const description = reactDesciptions[genRandomInt(2)]
+  
+    return (
+      <header>
+        <img src={reactImg} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {description} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+    )
+  }
+;
