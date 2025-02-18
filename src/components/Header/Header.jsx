d30e27cea56769ffeaa46cdbc2b2b75f93f41be7
Jared Ruiz <jared707@gmail.com>
2025-02-17 23:19:57 -0600

    Added css specific files for each component, organizing the styles to their respective component will help with structure later down the road.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import reactImg from '../../assets/react-core-concepts.png';
+import '../Header/Header.css';
 
 //array of descriptors to dynamically load
 const reactDesciptions = ["Fundamental", "Crucial", "Core"];
