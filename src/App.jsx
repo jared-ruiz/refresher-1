80af69a334b9b91d599bfcf596004a991ba2994f
Jared Ruiz <jared707@gmail.com>
2025-02-19 17:48:23 -0600

    Added a variable to try and change the content after each click but learned that the ui is not going to change after the inital rendering of the application unless we tell it to through state, which is what we will use next.

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,11 @@ import TabButton from './components/TabButton/TabButton.jsx';
 //... spread operator will pull all the key value pairs from the core_concepts object array and give the same results as the one above
 function App() {
 
+  let tabContent = 'Please click a button';
+
   function handleSelect(selectedButton) {
     //selectedButton => 'components', 'jsx', 'props', 'state'
-    console.log(selectedButton);
+    tabContent = selectedButton;
 }
 
 
@@ -40,7 +42,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          Dynamic Content
+          {tabContent}
         </section>
       </main>
     </div>
