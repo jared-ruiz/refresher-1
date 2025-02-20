7207b5443ca3ecebf7490a9d9fbab5e830493a96
Jared Ruiz <jared707@gmail.com>
2025-02-19 18:24:57 -0600

    Added new EXAMPLES nested objects to data.js to then pull those objects out and dynamically load them whenever the correct button is clicked on the site. Implemented state into the project as well.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { CORE_CONCEPTS } from './data.js';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton/TabButton.jsx';
+//react hooks useXYZ
+import { useState } from 'react';
 
 //added props to CoreConcepts component. Since I am familiar with this concept, I destructured them to make more sense visably for myself
 //... spread operator will pull all the key value pairs from the core_concepts object array and give the same results as the one above
 function App() {
 
-  let tabContent = 'Please click a button';
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
   function handleSelect(selectedButton) {
     //selectedButton => 'components', 'jsx', 'props', 'state'
-    tabContent = selectedButton;
+    setSelectedTopic(selectedButton);
+   
 }
 
 
@@ -42,7 +45,15 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {tabContent}
+            <div id='tab-content'>
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>
+                  {EXAMPLES[selectedTopic].code}
+                </code>
+              </pre>
+            </div>
         </section>
       </main>
     </div>
