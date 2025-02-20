55f98584ac790b394e5b1bd27f01536b6c26a7be
Jared Ruiz <jared707@gmail.com>
2025-02-19 20:27:18 -0600

    Added dynamic styling to buttons when clicked. Passed prop information to button component based on the true or false status of the state we current are working with in the app.jsx file.

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,11 +1,11 @@
 import '../TabButton/TabButton.css';
 
 //adding event listener as prop
-export default function TabButton({children, onSelect}) {
+export default function TabButton({children, onSelect, isSelected}) {
 
     return(
         <li>
-            <button onClick={onSelect}>{children}</button>
+            <button className={isSelected ? 'active' : ''} onClick={onSelect}>{children}</button>
         </li>
     )
 }
